0b47d0ccb14789c26ee20643afad812b11a98a9b
Than Zaw Toe <thanzawtoe.tzt@gmail.com>
2023-09-09 23:55:58 +0630

    06_chrono.cpp>> calculate fibonacci elapsed time:

--- 05_tuple.cpp
+++ 05_tuple.cpp
@@ -7,7 +7,7 @@ int main (){
     std::tuple<double, char , string > student1;
     using Student = std::tuple<double, char , string>;
     Student student2{1.4 , 'A', "Jose"};
-    // PrintStudent(student2);
+    PrintStudent(student2);
     std::cout << std::get<string>(student2) << std::endl;
     std::cout << std::get<2>(student2) << std::endl;
 
